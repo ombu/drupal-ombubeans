72cc976cb8efdfb849dc638eefd7f4651e825482
Jon Duell <jon@jonduell.com>
2014-04-14 10:46:22 -0700

    Hides entities in featured bean that are unpublished

--- featuredbean/FeaturedBean.class.php
+++ featuredbean/FeaturedBean.class.php
@@ -14,7 +14,16 @@ class FeaturedBean extends BeanPlugin {
    * Implements parent::view().
    */
   public function view($bean, $content, $view_mode = 'default', $langcode = NULL) {
-    $content['bean'][$bean->delta]['#featured_content'] = field_get_items('bean', $bean, 'field_featured_content');
+    $featured_content = field_get_items('bean', $bean, 'field_featured_content');
+
+    // Ensure that all nodes are published before printing.
+    foreach ($featured_content as $key => $value) {
+      if (!$value['entity']->status) {
+        unset($featured_content[$key]);
+      }
+    }
+
+    $content['bean'][$bean->delta]['#featured_content'] = $featured_content;
 
     // Allow bean styles to alter build.
     if (module_exists('bean_style')) {
