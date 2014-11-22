86aed6ce0e6ca5508d4dbdeb31f68ee75f563adc
Jon Duell <jon@jonduell.com>
2014-11-21 20:30:28 -0800

    Fixes problem with twitter block disappearing when moving in tiles

--- js/ombubeans-twitter-bean.js
+++ js/ombubeans-twitter-bean.js
@@ -1,7 +1,13 @@
 (function ($) {
 
-  Drupal.ajax.prototype.commands.twitter_bean_update = function(ajax, response, status) {
+  if (Drupal.ajax) {
+    Drupal.ajax.prototype.commands.twitter_bean_update = function(ajax, response, status) {
+      $.getScript('//platform.twitter.com/widgets.js');
+    };
+  }
+
+  $(document).on('tiles.requestSuccess', function(e, tile) {
     $.getScript('//platform.twitter.com/widgets.js');
-  };
+  });
 
 })(jQuery);
