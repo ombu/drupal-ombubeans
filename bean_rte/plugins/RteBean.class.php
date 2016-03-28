46f60881260831618d2ba08a8bf517cb3ca0667a
Jon Duell <jon@jonduell.com>
2016-03-28 11:36:21 -0700

    Fixes bean styles for RTE beans

--- bean_rte/plugins/RteBean.class.php
+++ bean_rte/plugins/RteBean.class.php
@@ -5,4 +5,17 @@
  * Rich Text Editor bean.
  */
 
-class RteBean extends BeanPlugin {}
+class RteBean extends BeanPlugin {
+  /**
+   * Implements the view method for this class
+   */
+  public function view($bean, $content, $view_mode = 'default', $langcode = NULL) {
+    // Allow bean styles to alter build.
+    if (module_exists('bean_style')) {
+      bean_style_view_alter($content, $bean);
+    }
+
+    return $content;
+  }
+
+}
