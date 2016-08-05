bb9601c07714e7c31b10ebc9176f54f6f8a5ef59
Andy Price <aprice42@gmail.com>
2016-08-05 11:06:15 -0700

    Adds ability to create bean styles for media beans - refs #18147

--- bean_media/plugins/MediaBean.class.php
+++ bean_media/plugins/MediaBean.class.php
@@ -5,4 +5,18 @@
  * Media Bean.
  */
 
-class MediaBean extends BeanPlugin {}
+class MediaBean extends BeanPlugin {
+  /**
+   * Implements parent::view().
+   */
+  public function view($bean, $content, $view_mode = 'default', $langcode = NULL) {
+    $content = parent::view($bean, $content, $view_mode, $langcode);
+
+    // Let any bean styles alter content.
+    if (module_exists('bean_style')) {
+      bean_style_view_alter($content, $bean);
+    }
+
+    return $content;
+  }
+}
