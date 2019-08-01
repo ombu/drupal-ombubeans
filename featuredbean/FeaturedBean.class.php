562afeeb188b3176714018a8d9af13911f3a43e0
Ben Hartig <ben.hartig@ombuweb.com>
2019-08-01 16:33:39 -0700

    Adds in extra fields for homepage feature bean.

--- featuredbean/FeaturedBean.class.php
+++ featuredbean/FeaturedBean.class.php
@@ -15,8 +15,10 @@ class FeaturedBean extends BeanPlugin {
    */
   public function view($bean, $content, $view_mode = 'default', $langcode = NULL) {
     $featured_content = field_get_items('bean', $bean, 'field_featured_content');
+    $featured_home_header = field_get_items('bean', $bean, 'field_featured_home_header');
+    $featured_home_content = field_get_items('bean', $bean, 'field_featured_home_content');
 
-    if ($featured_content) {
+    if ($featured_content || $featured_home_header || $featured_home_content) {
       // Ensure that all nodes are published before printing.
       foreach ($featured_content as $key => $value) {
         if (!$value['entity']->status) {
@@ -24,12 +26,40 @@ class FeaturedBean extends BeanPlugin {
         }
       }
 
+      foreach ($featured_home_header as $key => $value) {
+        if (!$value['entity']->status) {
+          unset($featured_home_header[$key]);
+        }
+      }
+
+      foreach ($featured_home_content as $key => $value) {
+        if (!$value['entity']->status) {
+          unset($featured_home_content[$key]);
+        }
+      }
+
       $content['bean'][$bean->delta]['#featured_content'] = $featured_content;
+      $content['bean'][$bean->delta]['#featured_home_header'] = $featured_home_header;
+      $content['bean'][$bean->delta]['#featured_home_content'] = $featured_home_content;
 
       // Allow bean styles to alter build.
       if (module_exists('bean_style')) {
         bean_style_view_alter($content, $bean);
       }
+
+      if ($bean->bean_style == 'callouthome') {
+        unset($content['bean'][$bean->delta]['#featured_content']);
+        unset($content['bean'][$bean->delta]['field_featured_content']);
+        $content['bean'][$bean->delta]['field_featured_home_header']['#weight'] = -10;
+        $content['bean'][$bean->delta]['field_featured_home_content']['#weight'] = 10;
+      }
+      else {
+        unset($content['bean'][$bean->delta]['#featured_home_header']);
+        unset($content['bean'][$bean->delta]['field_featured_home_header']);
+        unset($content['bean'][$bean->delta]['#featured_home_content']);
+        unset($content['bean'][$bean->delta]['field_featured_home_content']);
+      }
+
     }
 
     return $content;
